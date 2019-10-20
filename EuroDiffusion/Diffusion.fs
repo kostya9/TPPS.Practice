0192176f38cca172985c9d48a58550f1fe53cf05
kostya9 <kostya9@gmail.com>
2019-10-20 20:17:08 +0200

    Add diffusion implementation (without interface)

--- EuroDiffusion/Diffusion.fs
+++ EuroDiffusion/Diffusion.fs
@@ -1,2 +1,116 @@
 ﻿module EuroDiffusion.Diffusion
 
+type Country = {Name: string; XTop: int; YTop: int; XBot: int; YBot: int}
+
+type MotifCoins = {Amount: int; Country: Country}
+
+type City = {X: int; Y: int; Country: Country; Money: seq<MotifCoins>}
+
+type Result<'T> =
+    | Error of string
+    | Success of 'T
+    
+let private validateCoordinatesRange x y =
+    match (x, y) with
+    | (x, y) when x < 1 || x > 10 -> Error("Expected x to be in range of 1..10")
+    | (x, y) when y < 1 || y > 10 -> Error("Expected y to be in range of 1..10")
+    | (x, y) -> Success()
+
+let private validateCoordinates xBot yBot xTop yTop =
+    let validatedBot = validateCoordinatesRange xBot yBot
+    let validatedTop = validateCoordinatesRange xTop yTop
+    
+    match (validatedTop, validatedBot) with
+    | (Error(msg), _) -> Error(msg)
+    | (_, Error(msg)) -> Error(msg)
+    | (Success(), Success()) when xTop < xBot || yTop < yBot -> Error("Expecting xTop >= xBot, yTop >= yBot")
+    | (Success(), Success()) -> Success()
+
+let createCountry name xTop yTop xBot yBot =
+    let validationResult = validateCoordinates xTop yTop xBot yBot
+    
+    match validationResult with
+    | Success() -> Success({Name = name; XTop = xTop; YTop = yTop; XBot = xBot; YBot = yBot})
+    | Error(msg) -> Error(msg)
+
+let generateCitiesFromCountry country =
+    seq { for x in country.XTop .. country.XBot do
+            for y in country.YTop .. country.YBot do
+              yield {X = x; Y = y; Country = country; Money = [{Amount = 1_000_00; Country = country}]}
+        }
+    
+let private findCity cities x y =
+    Seq.tryFind (function c -> c.X = x && c.Y = y) cities 
+
+let createSimulationGrid cities =
+    [for y in 1..10 -> [for x in 1..10 -> findCity cities x y]]
+    
+let private cityWithUpdatedBudget (grid: City option list list) (x: int) (y: int) =
+    match grid.[y].[x] with
+    | Some city ->
+        
+        let neighborToRepresentation neighbor =
+            Seq.map (fun (m: MotifCoins) -> {Country = m.Country; Amount = m.Amount / 1000}) neighbor.Money
+            
+        let findNeighbors grid x y =
+             let possibleNeighbors =
+                  [|for dy in -1..1 do
+                      let targetY = y + dy
+                      if targetY < 0 || targetY >= List.length grid || targetY = y then
+                        yield None
+                      else                          
+                          let row = grid.[targetY]
+                          for dx in -1..1 do
+                              let targetX = x + dx
+                              if targetX < 0 || targetX >= List.length row || targetX = x then
+                                 yield None
+                              else
+                                  yield row.[x + dx]
+                  |]
+             Array.choose id possibleNeighbors
+             
+        let neighbors = findNeighbors grid x y 
+             
+        let updatedMoney =
+            neighbors
+            |> Seq.map neighborToRepresentation
+            |> Seq.collect (fun c -> c)
+            |> Seq.append city.Money
+            |> Seq.groupBy (fun c -> c.Country)
+            |> Seq.map (fun c -> snd c)
+            |> Seq.map (fun c -> {Country = (Seq.head c).Country; Amount = Seq.sumBy (fun c -> c.Amount) c})
+            |> Seq.map (fun c -> {Country = c.Country; Amount = c.Amount - Seq.length neighbors * c.Amount/1000})
+            |> Seq.filter (fun c -> c.Amount > 0)
+        
+        Some ({city with Money = updatedMoney })
+    | None -> None
+    
+let updateCurrencies (grid: City option list list) =
+    List.mapi (fun y r -> List.mapi (fun x c -> cityWithUpdatedBudget grid x y) grid.[y]) grid
+    
+    
+type StateOfComplete<'T> = {Item: 'T; Iteration: int}
+type GridCompleteState = {Cities: seq<StateOfComplete<City>>; IsFullyCompleted: bool }
+
+let createInitialCompleteState () =
+    {Cities = []; IsFullyCompleted = false}
+
+let checkCompleteness (grid: City option list list) (prevState: GridCompleteState) countriesNumber iteration =
+    let mutable isFullyCompleted = true
+    let cities = [|
+        for row in grid do
+            for city in Seq.choose id row do
+                match Seq.tryFind (fun c -> c.Item = city) prevState.Cities with
+                | Some(state) -> yield state
+                | None ->
+                    if Seq.length city.Money = countriesNumber then
+                        yield {Item = city; Iteration = iteration}
+                    else
+                        isFullyCompleted <- false
+    |]
+    
+    {Cities = cities; IsFullyCompleted = isFullyCompleted}
+    
+                
+            
+                
--- EuroDiffusion/Program.fs
+++ EuroDiffusion/Program.fs
@@ -1,8 +1,34 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open EuroDiffusion
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let france = Diffusion.createCountry "France" 1 4 4 6
+    let spain = Diffusion.createCountry "Spain" 3 1 6 3
+    let portugal = Diffusion.createCountry "Portugal" 1 1 2 2
+    
+    let countries = [|france; spain; portugal|]
+    
+    let countries =
+        [|for c in countries do
+            match c with
+            | Diffusion.Success(country) -> yield country
+            | Diffusion.Error(e) ->
+                failwith e
+                |]
+        
+    let mutable grid =
+        countries
+        |> Seq.map Diffusion.generateCitiesFromCountry
+        |> Seq.collect (fun c -> c)
+        |> Diffusion.createSimulationGrid
+        
+    let mutable completeness = Diffusion.createInitialCompleteState ()
+    let mutable iteration = 0
+    
+    while not completeness.IsFullyCompleted do
+        grid <- Diffusion.updateCurrencies grid
+        completeness <- Diffusion.checkCompleteness grid completeness 3 iteration
+        iteration <- iteration + 1
+    
+
+    0
